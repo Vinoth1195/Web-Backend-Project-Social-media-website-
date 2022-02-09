ab264d203b64bfa6052e17da008ba8edc4b658c3
vinoth <vinoth9511@gmail.com>
2022-02-09 17:37:38 -0500

    "Added CORS"

--- venv1/app/main.py
+++ venv1/app/main.py
@@ -8,14 +8,14 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
 
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+origins=["*"]
+app.add_middleware(
+     CORSMiddleware,
+     allow_origins=origins,
+     allow_credentials=True,
+     allow_methods=["*"],
+     allow_headers=["*"],
+ )
 
 
 app.include_router(Posts.router)
